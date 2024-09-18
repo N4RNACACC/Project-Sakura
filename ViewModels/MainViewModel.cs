83bf54991fdc5203c801def4456b7ca28a9db39a
N4RSONIC <colorcatcc@outlook.com>
2024-09-18 16:30:49 +0800

    添加项目文件。

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SakuraCraftLauncher.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace SakuraCraftLauncher.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
