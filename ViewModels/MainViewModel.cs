e671591cecb5142fd7755f257b56eacaa9c1b852
N4RSONIC <colorcatcc@outlook.com>
2024-09-18 19:43:32 +0800

    亚克力窗口背景及标题栏实现

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace SakuraCraftLauncher.ViewModels;
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace SakuraCraftLauncher.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public ICommand GameLaunch { get; }
+
+    public MainViewModel()
+    {
+        GameLaunch = ReactiveCommand.Create(() =>
+        {
+            // 当按钮被点击时，这里的代码将被执行。
+        });
+    }
 }
+
