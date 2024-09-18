6957805609fe0f52ce7a9b9479943356d1f34d80
N4RSONIC <colorcatcc@outlook.com>
2024-09-19 00:18:19 +0800

    基本启动实现

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,5 +1,11 @@
 ﻿using ReactiveUI;
 using System.Windows.Input;
+using System.Threading.Tasks;
+using MinecraftLaunch.Components.Authenticator;
+using MinecraftLaunch.Components.Resolver;
+using MinecraftLaunch.Classes.Interfaces;
+using MinecraftLaunch.Classes.Models.Launch;
+using MinecraftLaunch.Components.Launcher;
 
 namespace SakuraCraftLauncher.ViewModels;
 
@@ -9,10 +15,35 @@ public class MainViewModel : ViewModelBase
 
     public MainViewModel()
     {
-        GameLaunch = ReactiveCommand.Create(() =>
+        GameLaunch = ReactiveCommand.Create(static async () =>
         {
-            // 当按钮被点击时，这里的代码将被执行。
+            // 登录的账户信息
+            var loginaccount = new OfflineAuthenticator("TEST").Authenticate();
+            IGameResolver resolver = new GameResolver(".minecraft");
+            // 启动配置
+            var config = new LaunchConfig
+            {
+                // 设置启动的账户
+                Account = loginaccount,
+                // 配置代码是否可独立运行
+                IsEnableIndependencyCore = true,
+                // Java配置
+                JvmConfig = new(@"C:\Program Files\Java\jre1.8.0_421\bin\javaw.exe")
+                {
+                    MaxMemory = 4096,
+                    MinMemory = 1024,
+                },
+            };
+            // 启动核心所需的信息
+            Launcher launcher = new(resolver, config);
+            // 异步启动游戏，防止启动器卡死
+            await Task.Run(async () => {
+                var gameProcessWatcher = await launcher.LaunchAsync("1.12.2");
+                // 2024-9-19 简单启动实现，已满足某些方面的基础需要
+                // 2024-9-19 请确保与启动器同目录的.minecraft文件夹存在版本“1.12.2”，否则当启动器START按钮按下后程序会因为此段代码崩溃，待后续完善
+            });
         });
+
     }
 }
 
